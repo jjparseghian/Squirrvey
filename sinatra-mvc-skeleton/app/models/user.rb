770c5650d0b4b538ee751fab1ab90011bf1d92bd
Vivek Ratkalkar <vratkalkar@live.com>
2015-02-13 15:13:19 -0500

    Added basic user auth and login/signup forms

--- sinatra-mvc-skeleton/app/models/user.rb
+++ sinatra-mvc-skeleton/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  # Remember to create a migration!
+
   has_many :surveys
   has_many :user_answers
   has_many :answers, through: :user_answers
